d50558d5057fb6c8eb6a93b25eb195b4ba36af2c
Yasar Cakir <dev.yasarcakir@gmail.com>
2025-08-24 22:34:35 +0700

    feat: update Header component to toggle between light and dark mode with appropriate icons

--- src/components/layouts/form-elements/input.tsx
+++ src/components/layouts/form-elements/input.tsx
@@ -7,7 +7,7 @@ type InputProps = {
 export const Input = ({ icon, ...props }: InputProps) => {
   return (
     <div className="flex w-full max-w-md items-center rounded-md border border-gray-300 shadow-md dark:border-dark-element dark:bg-dark-element dark:text-white">
-      <span className="ml-2">{icon}</span>
+      <span className="ml-2 dark:text-gray-500">{icon}</span>
       <input
         className={cn("ml-2 border-none p-2 outline-none dark:placeholder:text-gray-500", props.className)}
         {...props}
--- src/components/layouts/header/header.tsx
+++ src/components/layouts/header/header.tsx
@@ -1,14 +1,22 @@
-import { HiOutlineMoon } from "react-icons/hi";
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi";
+
 import { useUIStore } from "@/stores/ui-store";
 
 export const Header = () => {
   const toggleTheme = useUIStore((state) => state.toggleTheme);
+  const theme = useUIStore((state) => state.theme);
 
   return (
-    <header className="w-full shadow-md dark:text-white dark:bg-dark-element bg-light-element z-10 transition-colors duration-300">
-      <section className="container p-4 flex justify-between items-center mx-auto">
-        <h1 className="font-semibold text-xl">Where in the world?</h1>
-        <button type="button" className="flex items-center gap-2 cursor-pointer px-4 py-2 text-md" onClick={toggleTheme}><HiOutlineMoon /> Dark Mode</button>
+    <header className="bg-light-element z-10 w-full shadow-md transition-colors duration-300 dark:bg-dark-element dark:text-white">
+      <section className="container mx-auto flex items-center justify-between p-4">
+        <h1 className="text-xl font-semibold">Where in the world?</h1>
+        <button
+          type="button"
+          className="text-md flex cursor-pointer items-center gap-2 px-4 py-2"
+          onClick={toggleTheme}
+        >
+          {theme === "dark" ? <HiOutlineSun /> : <HiOutlineMoon />} {theme === "dark" ? "Light" : "Dark"} Mode
+        </button>
       </section>
     </header>
   );
