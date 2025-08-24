a217446ef3acf5878ea69ebc775da601a9bfeb7c
Yasar Cakir <dev.yasarcakir@gmail.com>
2025-08-24 22:08:22 +0700

    feat: enhance Input and Select components with improved styling and functionality

--- src/components/layouts/filters/input.tsx
+++ src/components/layouts/filters/input.tsx
@@ -6,9 +6,12 @@ type InputProps = {
 
 export const Input = ({ icon, ...props }: InputProps) => {
   return (
-    <div className="flex w-full max-w-md items-center rounded-md border border-gray-300 p-2">
-      {icon}
-      <input className={cn("ml-2 border-none p-2 outline-none", props.className)} {...props} />
+    <div className="flex w-full max-w-md items-center rounded-md border border-gray-300 dark:border-dark-element dark:bg-dark-element dark:text-white">
+      <span className="ml-2">{icon}</span>
+      <input
+        className={cn("ml-2 border-none p-2 outline-none dark:placeholder:text-white", props.className)}
+        {...props}
+      />
     </div>
   );
 };
--- src/components/layouts/filters/select.tsx
+++ src/components/layouts/filters/select.tsx
@@ -1,6 +1,7 @@
+import { useEffect, useRef, useState } from "react";
+
 import { HiChevronDown } from "react-icons/hi";
 import { cn } from "@/utils/classname";
-import { useState } from "react";
 
 type SelectProps<T> = {
   className?: string;
@@ -12,61 +13,78 @@ type SelectProps<T> = {
 
 export const Select = <T,>({ elements, ...props }: SelectProps<T>) => {
   const [isOpen, setIsOpen] = useState(false);
-  return (
-    <div className="relative dark:text-white">
-      <button className="z-50 cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
-        <div className={cn("relative w-60 p-0", props.className)}>
-          <HiChevronDown
-            className={cn(
-              "absolute top-0 right-0 z-30 h-6 w-6 -translate-x-1/4 translate-y-1/3 transition-transform duration-300",
-              {
-                "rotate-180": isOpen,
-              }
-            )}
-          />
+  const ref = useRef<HTMLDivElement>(null);
 
-          <div className="absolute right-0 left-0 m-0 mx-auto h-10 w-60 rounded-md shadow-md dark:bg-dark-element">
-            <p className="mt-2 flex items-center px-2">
-              {props?.value ? (
-                <span>{elements.find((el) => el.value === props.value)?.label}</span>
-              ) : (
-                <span className="text-gray-500">{props.placeholder}</span>
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className="mr-60 mb-[4rem] h-0 dark:text-white" ref={ref}>
+      <button className="z-50 cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+        <div className="relative">
+          {" "}
+          <div className={cn("absolute top-0 right-0 left-0 w-60 p-0", props.className)}>
+            <HiChevronDown
+              className={cn(
+                "absolute top-0 right-0 z-30 h-6 w-6 -translate-x-1/4 translate-y-1/3 transition-transform duration-300",
+                {
+                  "rotate-180": isOpen,
+                }
               )}
-            </p>
-            <ul
-              className={cn("z-30 mt-4 overflow-y-auto shadow-md dark:bg-dark-element dark:text-white", {
-                hidden: !isOpen,
-              })}
-            >
-              {elements?.length > 0 ? (
-                elements.map((element) => (
-                  <li
-                    key={element.label}
-                    className={cn("w-full cursor-pointer p-2 hover:bg-gray-100 dark:hover:bg-dark-bg", {
-                      ["bg-gray-100 dark:bg-dark-bg"]: element.value === props.value,
-                      ["dark:text-bg-transparent cursor-default text-gray-400 hover:bg-transparent"]:
-                        element.value === "",
-                    })}
-                  >
-                    <button
-                      className="w-full cursor-pointer text-left"
-                      type="button"
-                      onClick={() => {
-                        if (element.value === props.value) {
-                          return;
-                        }
-                        props.onChange(element.value);
-                        setIsOpen(false);
-                      }}
+            />
+
+            <div className="absolute top-0 right-0 left-0 m-0 mx-auto h-10 w-60 rounded-md shadow-md dark:bg-dark-element">
+              <p className="flex items-center px-2 pt-2">
+                {props?.value ? (
+                  <span>{elements.find((el) => el.value === props.value)?.label}</span>
+                ) : (
+                  <span className="text-gray-500">{props.placeholder}</span>
+                )}
+              </p>
+              <ul
+                className={cn("z-30 mt-4 overflow-y-auto shadow-md dark:bg-dark-element dark:text-white", {
+                  hidden: !isOpen,
+                })}
+              >
+                {elements?.length > 0 ? (
+                  elements.map((element) => (
+                    <li
+                      key={element.label}
+                      className={cn("w-full cursor-pointer p-2 hover:bg-gray-100 dark:hover:bg-dark-bg", {
+                        ["bg-gray-100 dark:bg-dark-bg"]: element.value === props.value,
+                        ["dark:text-bg-transparent cursor-default text-gray-400 hover:bg-transparent"]:
+                          element.value === "",
+                      })}
                     >
-                      {element.label}
-                    </button>
-                  </li>
-                ))
-              ) : (
-                <li>No options available</li>
-              )}
-            </ul>
+                      <button
+                        className="w-full cursor-pointer text-left"
+                        type="button"
+                        onClick={() => {
+                          if (element.value === props.value) {
+                            return;
+                          }
+                          props.onChange(element.value);
+                          setIsOpen(false);
+                        }}
+                      >
+                        {element.label}
+                      </button>
+                    </li>
+                  ))
+                ) : (
+                  <li>No options available</li>
+                )}
+              </ul>
+            </div>
           </div>
         </div>
       </button>
